c9e6ecb402e0436086485b36d01f724c971530b5
kasinsparks <kasinsparks@gmail.com>
2024-03-12 17:12:21 -0400

    Added Windows support

---
+++ cen3031_team2.cpp
@@ -0,0 +1,12 @@
+﻿// cen3031_team2.cpp : Defines the entry point for the application.
+//
+
+#include "cen3031_team2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
